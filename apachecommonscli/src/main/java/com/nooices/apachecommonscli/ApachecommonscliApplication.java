aeac25014ffc60b6db8ef8479c694457452e9a85
wangwenyao <wangwenyao@changingedu.com>
2021-04-07 17:53:17 +0800

    init project

---
+++ apachecommonscli/src/main/java/com/nooices/apachecommonscli/ApachecommonscliApplication.java
@@ -0,0 +1,13 @@
+package com.nooices.apachecommonscli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachecommonscliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApachecommonscliApplication.class, args);
+    }
+
+}
---
+++ apachecommonscli/src/test/java/com/nooices/apachecommonscli/ApachecommonscliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nooices.apachecommonscli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApachecommonscliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
